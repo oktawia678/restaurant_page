1ab1c904c36e7b0be0553fa5ba1e7f51f3acc378
Hubert Wozniak <oktawia_k@wp.pl>
2022-08-09 00:39:36 +0200

    styling home page

--- src/components/section-center/SectionCenter.js
+++ src/components/section-center/SectionCenter.js
@@ -1,30 +1,17 @@
 import { Button } from "../button/Button";
+import "./section-center.css";
 export const SectionCenter = () => {
   return (
-    <div
-      style={{
-        display: "flex",
-        backgroundColor: "blue",
-        width: "100vw",
-        height: "85vh",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <div
-        style={{
-          display: "flex",
-          backgroundColor: "green",
-          width: "70%",
-          height: "40%",
-          flexDirection:'column'
-        }}
-      >
-        <div style={{ display: "flex",backgroundColor:'yellow',flex:'1',justifyContent:'center',alignItems:'center' }}>WE`RE GOREAMEX RESTAURANT</div>
-        <div style={{ display: "flex",backgroundColor:'violet',flex:'1',justifyContent:'center',alignItems:'center' }}>MAKING DELICIOUS PREMIUM FOOD</div>
-        <div style={{ display: "flex",backgroundColor:'pink',flex:'1',justifyContent:'center',alignItems:'center' }}>Welcome to our restaurant! If you want to make a reservation, please, push the button below. Enjoy your meal!</div>
-        <div style={{ display: "flex",backgroundColor:'brown',flex:'1',justifyContent:'center',alignItems:'center' }}>
-            <Button/>
+    <div className="container">
+      <div className="container_for_four_elements">
+        <div className="title_one">WE`RE GOREAMEX RESTAURANT</div>
+        <div className="title_two">MAKING DELICIOUS <br/> PREMIUM FOOD</div>
+        <div className="title_three">
+          Welcome to our restaurant! If you want to make a reservation, please, <br/>
+          push the button below. Enjoy your meal!
+        </div>
+        <div className="title_four">
+          <Button />
         </div>
       </div>
     </div>
