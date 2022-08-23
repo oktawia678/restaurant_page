ddda38253a6e88d8b731c0881a30eb5b81b612d0
Oktawia Kustra <oktawia_k@wp.pl>
2022-08-23 23:15:05 +0200

    create component Menu

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Home } from './pages/home/Home';
 import { Contact } from './pages/contact/Contact';
 import { Routes, Route } from "react-router-dom";
+import {Menu} from "./pages/Menu/Menu";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="contact" element={<Contact/>} />
+      <Route path="Menu" element={<Menu/>}/>
     </Routes>
   );
 }
