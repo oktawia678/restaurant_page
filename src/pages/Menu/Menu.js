999da7ecc72395c9d3389cd065a8733dea3b7b53
Oktawia Kustra <oktawia_k@wp.pl>
2022-08-28 23:45:04 +0200

    creating a menu offer

--- src/pages/Menu/Menu.js
+++ src/pages/Menu/Menu.js
@@ -1,16 +1,29 @@
+import './Menu.css'
 import { Header } from "../../components/header/Header"
 export const Menu  = () => {
+
+  
 return(
     <>
     <Header/>
     <div className="container_menu_card">
+        <div className='offer_left'>
+            <div className='option_dish_one'></div>
+            <div className='option_dish_two'></div>
+            <div className='option_dish_three'></div>
+            
+        </div>
+        <div className='offer_right'>
+            <div className='option_dish_four'></div>
+            <div className='option_dish_five'></div>
+            <div className='option_dish_six'></div>
+        </div>
+
 
     </div>
     
     </>
     )
-    
-
     
     
     
