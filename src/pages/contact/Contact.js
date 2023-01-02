0315dc1527e98e1ebccccbf42108c88bf869cdc9
oktawia678 <oktawia_k@wp.pl>
2023-01-02 18:09:44 +0100

    add request to server

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -1,14 +1,21 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/header/Header";
 import "./contact.scss";
 export const Contact = () => {
+  const [welcomeElephants, setWelcomeElephants] = useState("");
+  useEffect(() => {
+    fetchData();
+  }, []);
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:4000/elephants");
+    const {data} = await response.json();
+    setWelcomeElephants(data);
+  };
   return (
     <div className="contact_main_container">
       <Header />
       <div className="contact_container">
-        <div className="section_left">
-          We are open daily, we serve seasonal food, including fish and meat.
-          All freshly cooked.
-        </div>
+        <div className="section_left">{welcomeElephants}</div>
         <div className="section_right">
           <div className="header_container">
             <div className="header">(123) 456-7890 / contact@gmail.com</div>
